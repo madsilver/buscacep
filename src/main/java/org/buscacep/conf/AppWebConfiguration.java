94cd2b246ece1285e80391608c3b9cf1d491c94c
madsilver <rodrigo-prata@hotmail.com>
2015-11-07 22:37:51 -0200

    Criação do projeto.

---
+++ src/main/java/org/buscacep/conf/AppWebConfiguration.java
@@ -0,0 +1,22 @@
+package org.buscacep.conf;
+
+import org.buscacep.controllers.HomeController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
